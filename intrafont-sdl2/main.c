38581372a13fba890c9ea81044bd08886b648a8f
Dima Pulkinen <dima@itsemast.com>
2024-06-20 10:58:04 +0300

    trying to replicate how intrafont is rendered internally

--- intrafont-sdl2/main.c
+++ intrafont-sdl2/main.c
@@ -2,7 +2,25 @@
 
 #include <intraFont.h>
 
-#include <GL/gl.h>
+enum colors
+{
+  /* ARGB */
+  RED = 0xFF0000FF,
+  GREEN = 0xFF00FF00,
+  BLUE = 0xFFFF0000,
+  WHITE = 0xFFFFFFFF,
+  LITEGRAY = 0xFFBFBFBF,
+  GRAY = 0xFF7F7F7F,
+  DARKGRAY = 0xFF3F3F3F,
+  BLACK = 0xFF000000,
+};
+
+struct fontVertex
+{
+  float u, v;
+  unsigned int c;
+  float x, y, z;
+};
 
 int main(int argc, char *argv[])
 {
@@ -23,8 +41,8 @@ int main(int argc, char *argv[])
 
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-  SDL_Rect rect = {0, 0, 480, 272};
-  SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 480, 272);
+  SDL_Rect rect = {0, 0, font->texWidth, font->texHeight};
+  SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, font->texWidth, font->texHeight);
 
   int running = 1;
   SDL_Event event;
@@ -49,17 +67,35 @@ int main(int argc, char *argv[])
 
     SDL_RenderClear(renderer);
 
-    intraFontSetStyle(font, 1.0f, 0xFF000000, 0xFFFFFFFF, 0.0f, INTRAFONT_ALIGN_CENTER);
+    intraFontSetStyle(font, 1.0f, BLACK, WHITE, 0.0f, INTRAFONT_ALIGN_CENTER);
     intraFontPrint(font, 240, 240, "Hello PSP!");
 
-    void *pixels;
-    int pitch;
-    SDL_LockTexture(texture, &rect, &pixels, &pitch);
-    glReadPixels(0, 0, 480, 272, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-    SDL_UnlockTexture(texture);
+    SDL_UpdateTexture(texture, &rect, font->texture, rect.w * sizeof(Uint32));
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_Vertex v[font->v_size];
+
+    for (int i = 0; i < font->v_size; i++)
+    {
+      // printf("v[%d].u = %0.2f (tex u) ", i, font->v[i].u);
+      // printf("v[%d].v = %0.2f (tex v) ", i, font->v[i].v);
+      // printf("v[%d].c = %x (rgba8888) ", i, font->v[i].c);
+      // printf("v[%d].x = %2.2f ", i, font->v[i].x);
+      // printf("v[%d].y = %2.2f ", i, font->v[i].y);
+      // printf("v[%d].z = %2.2f ", i, font->v[i].z);
+      // printf("\n");
+
+      struct fontVertex fv = font->v[i];
+      SDL_Color c = {255, 255, 255, 255};
+      SDL_FPoint p = {fv.x, fv.y};
+      SDL_FPoint t = {fv.u, fv.v};
+      v[i] = (SDL_Vertex){p, c, t};
+      SDL_RenderDrawPointF(renderer, p.x, p.y);
+      SDL_RenderDrawPointF(renderer, t.x * rect.w, t.y * rect.h);
+    }
+
+    // SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    // SDL_RenderCopy(renderer, texture, &rect, &rect);
+    SDL_RenderGeometry(renderer, texture, v, 1 * 4, NULL, 0);
     SDL_RenderPresent(renderer);
   }
 
